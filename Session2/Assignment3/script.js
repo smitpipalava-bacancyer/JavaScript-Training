e3da511c171faebd555abfc909396bfed0858906
S-tech12 <smitpipalava@gmail.com>
2026-01-27 17:10:41 +0530

    Added second method in question of roles

--- Session2/Assignment3/script.js
+++ Session2/Assignment3/script.js
@@ -6,22 +6,35 @@ button.addEventListener("click", () => {
 
     let x = role.value.toUpperCase();
 
-    switch (true){
-        case x === "ADMIN":
-            resultDiv.innerHTML = "Full Access";
-            resultDiv.style.color = "green";
-            break;
-        case x === "USER":
-            resultDiv.innerHTML = "Limited access";
-            resultDiv.style.color = "#673AB7";
-            break;
-        case x === "MANAGER":
-            resultDiv.innerHTML = "Moderate Access";
-            resultDiv.style.color = "orange";
-            break;
-        default : 
-            resultDiv.innerHTML = "Invalid role"
-            resultDiv.style.color = "red";
-            break;
+    // switch (true){
+    //     case x === "ADMIN":
+    //         resultDiv.innerHTML = "Full Access";
+    //         resultDiv.style.color = "green";
+    //         break;
+    //     case x === "USER":
+    //         resultDiv.innerHTML = "Limited access";
+    //         resultDiv.style.color = "#673AB7";
+    //         break;
+    //     case x === "MANAGER":
+    //         resultDiv.innerHTML = "Moderate Access";
+    //         resultDiv.style.color = "orange";
+    //         break;
+    //     default : 
+    //         resultDiv.innerHTML = "Invalid role"
+    //         resultDiv.style.color = "red";
+    //         break;
+    // }
+
+    // second and easiest method
+    let roles = {
+        ADMIN : "Full Access",
+        USER : "Limited Access",
+        MANAGER : "Moderate Access",
+        DEFAULT : "Invalid Role"
     }
+
+    const result = function checkStatus(){
+        return roles[x || "default"];
+    }
+    resultDiv.innerHTML = result(); 
 })
